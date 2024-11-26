a841fbbc6d84e58d1a0ca2544de8e09754f557cc
hanniel09 <hannielvieira1227@gmail.com>
2024-11-26 16:44:51 -0300

    feat: update user services to use usersDTO

--- src/main/kotlin/com/userregister/services/UserService.kt
+++ src/main/kotlin/com/userregister/services/UserService.kt
@@ -1,6 +1,8 @@
 package com.userregister.services
 
 import com.userregister.domain.users.User
+import com.userregister.domain.users.UserRequestDTO
+import com.userregister.domain.users.UserResponseDTO
 import com.userregister.repositories.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -8,27 +10,50 @@ import java.util.*
 
 @Service
 class UserService(@Autowired val userRepository: UserRepository) {
-    fun getAllUsers(): List<User> {
-        return userRepository.findAll().toList()
+    fun getAllUsers(): List<UserResponseDTO> {
+       val  users: List<User> = userRepository.findAll().toList()
+        return users.map {
+            user -> UserResponseDTO(
+                id = user.id ?: throw IllegalArgumentException("User ID cannot be null"),
+                name = user.name,
+                email = user.email,
+            )
+        }
     }
 
-    fun getUserById(id: UUID): User {
-        return userRepository.findById(id).orElse(null)
+    fun getUserById(id: UUID): UserResponseDTO {
+        val user: User = userRepository.findById(id).orElse(null)
             ?: throw RuntimeException("User with id: $id wasn't found")
+
+        return UserResponseDTO(
+            id = user.id ?: throw IllegalArgumentException("User ID cannot be null"),
+            name = user.name,
+            email = user.email,
+        )
     }
 
-    fun createUser(user: User): User {
-        val createdUser = userRepository.save(user)
-        return createdUser
+    fun createUser(userRequestDTO: UserRequestDTO): User {
+        val user  = User(
+            name = userRequestDTO.name,
+            email = userRequestDTO.email,
+        )
+        return userRepository.save(user)
     }
 
-    fun updateUser(updatedUser: User, id: UUID): User {
-       val existingUser = getUserById(id)
+    fun updateUser(updatedUser: UserRequestDTO, id: UUID): UserResponseDTO {
+       val existingUser:User = userRepository.findById(id).orElse(null)
+           ?: throw RuntimeException("User with id: $id wasn't found")
 
         existingUser.name = updatedUser.name
         existingUser.email = updatedUser.email
 
-        return userRepository.save(existingUser)
+        val savedUser = userRepository.save(existingUser)
+
+        return UserResponseDTO(
+            id = savedUser.id ?: throw IllegalArgumentException("User with id: $id wasn't found"),
+            name = savedUser.name,
+            email = savedUser.email
+        )
 
     }
 
