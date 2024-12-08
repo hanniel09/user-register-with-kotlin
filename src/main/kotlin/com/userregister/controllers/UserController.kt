d59bffe3f209290ac9612a998f60a8083cf93cb2
hanniel09 <hannielvieira1227@gmail.com>
2024-12-08 17:47:44 -0300

    refact: refact UserController.kt

--- src/main/kotlin/com/userregister/controllers/UserController.kt
+++ src/main/kotlin/com/userregister/controllers/UserController.kt
@@ -3,7 +3,7 @@ package com.userregister.controllers
 import com.userregister.domain.users.User
 import com.userregister.domain.users.UserRequestDTO
 import com.userregister.domain.users.UserResponseDTO
-import com.userregister.services.UserService
+import com.userregister.services.UsersService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
@@ -13,35 +13,35 @@ import java.util.UUID
 
 @RestController()
 @RequestMapping("/api/users")
-class UsersController (@Autowired val userService: UserService) {
+class UsersController (@Autowired val usersService: UsersService) {
 
     @GetMapping
     fun getAllUsers(): ResponseEntity<List<UserResponseDTO>> {
-        val users = userService.getAllUsers()
+        val users = usersService.getAllUsers()
         return ResponseEntity(users, HttpStatus.OK)
     }
 
     @GetMapping("/{id}")
     fun getUserById(@PathVariable("id") id: UUID): ResponseEntity<UserResponseDTO> {
-        val user = userService.getUserById(id)
+        val user = usersService.getUserById(id)
         return ResponseEntity(user, HttpStatus.OK)
     }
 
     @PostMapping("/register")
     fun createUser(@RequestBody @Valid userRequestDTO: UserRequestDTO): ResponseEntity<User> {
-        val user =userService.createUser(userRequestDTO)
+        val user =usersService.createUser(userRequestDTO)
         return ResponseEntity(user, HttpStatus.CREATED)
     }
 
     @PutMapping("/{id}")
     fun updateUser(@RequestBody @Valid userRequestDTO: UserRequestDTO, @PathVariable id:UUID): ResponseEntity<UserResponseDTO> {
-        val user = userService.updateUser(userRequestDTO, id)
+        val user = usersService.updateUser(userRequestDTO, id)
         return ResponseEntity(user, HttpStatus.OK)
     }
 
     @DeleteMapping("/{id}")
     fun deleteUser(@PathVariable id: UUID): ResponseEntity<Void> {
-        userService.deleteUserById(id)
+        usersService.deleteUserById(id)
         return ResponseEntity<Void>(HttpStatus.NO_CONTENT)
     }
 }
